f52a675774ffbf29ca94f593b0aa13692cfa3c0d
Rogelio Trejo <rouge2504@gmail.com>
2023-08-01 14:22:54 -0600

    Login

--- gm_frontend/lib/src/pages/login/login_page.dart
+++ gm_frontend/lib/src/pages/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,8 +7,72 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LOGIN PAGE'),
+      bottomNavigationBar: Container(
+        height: 50,
+        child: _textDontHaveAccount(),
+      ),
+      body: Stack(
+        children: [
+          _backgroundCover(context),
+          Column(
+            children: [_imageCover(), _textAppName()],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _backgroundCover(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.4,
+      color: Colors.amber,
+      //alignment: Alignment.topCenter,
+    );
+  }
+
+  Widget _textAppName() {
+    return Text(
+      'DELIVERY MYSQL',
+      style: TextStyle(
+          fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+    );
+  }
+
+  Widget _textDontHaveAccount() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '¿No tienes cuenta?',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 17,
+          ),
+        ),
+        SizedBox(width: 7),
+        Text(
+          'Regristrate aqui',
+          style: TextStyle(
+            color: Colors.amber,
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _imageCover() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 20, bottom: 15),
+        alignment: Alignment.center,
+        child: Image.asset(
+          'assets/img/delivery.png',
+          width: 130,
+          height: 130,
+        ),
       ),
     );
   }
