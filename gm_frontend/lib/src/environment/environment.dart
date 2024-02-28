771df6b8ef073a7b2a68892297cd356f96622046
Rogelio Trejo <rouge2504@gmail.com>
2024-02-27 20:10:40 -0600

    Add ip

--- gm_frontend/lib/src/environment/environment.dart
+++ gm_frontend/lib/src/environment/environment.dart
@@ -6,6 +6,7 @@ class Environment {
       "TEST-8253011200569563-072319-c7a718c5097c269fb5dd529b71dda761-72540198";
   static const String PUBLIC_KEY = 'TEST-f4e7b224-75e9-4cfe-a0d5-f89319ee809f';
   static Uri EMERGENCY_CHAT = Uri.parse('https://wa.me/5531169270?');
+//  static Uri EMERGENCY_CHAT = Uri.parse('https://wa.me/5540713955?');
   static const String API_KEY_MAPS = 'AIzaSyCsqZdaMbk3Azx2C96K_RzBJgkqW35jrck';
   static const String UNIVERSAL_RADIO = 'https://universal881.com/';
   static const String MIX_RADIO =
